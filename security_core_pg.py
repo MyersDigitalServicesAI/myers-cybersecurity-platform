46b2564aad51ffe0478fb0a9c44ba2dc60d73280
Dustin Myers <MyersDigitalServicesAI@gmail.com>
2025-06-09 01:56:05 -0400

    security_core_pg.py

--- security_core_pg.py
+++ security_core_pg.py
@@ -113,7 +113,7 @@ class SecurityCore:
             pass
         
        # Users table
-       cursor.execute('''
+           cursor.execute('''
        CREATE TABLE IF NOT EXISTS users (
            id VARCHAR(255) PRIMARY KEY,
            email VARCHAR(255) UNIQUE NOT NULL,
